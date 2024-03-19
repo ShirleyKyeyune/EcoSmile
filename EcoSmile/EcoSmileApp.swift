30fb01859ab7bf1053ca9305003668376b8ba9be
Shirley Kyeyune <GKyeyune@oldmutual.com>
2024-03-19 09:45:58 +0300

    Initial Commit

---
+++ EcoSmile/EcoSmileApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoSmileApp.swift
+//  EcoSmile
+//
+//  Created by Shirley Kyeyune on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoSmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
